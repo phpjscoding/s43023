1a248a4b0186b75957fae77a4f14bb2b2b11b704
ismail outaleb <windowofcoding@gmail.com>
2023-05-30 22:03:44 +0000

    aded flexoffers text in footer

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -3,5 +3,6 @@
     class="mx-auto bottom-0 mt-10 items-center justify-center flex flex-col sm:flex-row space-x-2 p-4 text-xs"
   >
     <p class="font-thin">{{ $t("Footer") }}</p>
+    <smal> flexoffers </smal>
   </footer>
 </template>
